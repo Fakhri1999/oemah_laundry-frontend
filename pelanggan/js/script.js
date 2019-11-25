724e3d32767fc97f6cf0d446563e4f01686be9a9
Wildanokt <oktavianwww@gmail.com>
2019-11-25 14:56:12 +0700

    fix code

--- pelanggan/js/script.js
+++ pelanggan/js/script.js
@@ -20,6 +20,11 @@ $('#nav-profile').on('click', function (e) {
     e.preventDefault()
     window.location.replace("./profile.html")
 })
+$('#nav-logout').on('click', function (e) {
+    e.preventDefault()
+    localStorage.setItem('status', 0)
+    window.location.replace("./index.html")
+})
 
 //tambah pesanan
 $('#tambah-pesanan').on('click', function () {
@@ -32,17 +37,66 @@ var check = {
     authCheck: function () {
         if (localStorage.getItem('status') == 0) {
             window.location.replace("./index.html")
-        } else {
-            window.localStorage.replace("./home.html")
         }
     }
 }
 
 $('#form-login').submit(function () {
+    let username = $('input#username').val()
+    let pass = $('input#pass').val()
+
+
     localStorage.setItem('status', 1)
+    let data = {
+        'username': username,
+    }
+    localStorage.setItem('userdata', JSON.stringify(data))
+    //cara pakai => JSON.parse(localStorage.getItem('userdata'))
     window.location.replace("./home.html")
 })
 
+//----------------------------------- pesanan ---------------------------------------
 
+var pesanan = {
+    load: function () {
+        $.ajax({
+            url: link,
+            type: 'get',
+            beforeSend: function () {
+                $.mobile.loading('show', {
+                    text: 'Loading data...',
+                    textVisible: true
+                })
+            },
+            success: function (dataObject) {
+                $dataobject = {
+                    1: {
+                        'id': 1,
+                        'barang_cucian': 'kaos'
+                    },
+                    2: {
+                        'id': 2,
+                        'barang_cucian': 'kaos aaa'
+                    },
+                    3: {
+                        'id': 3,
+                        'barang_cucian': 'kaos'
+                    },
+                    4: {
+                        'id': 4,
+                        'barang_cucian': 'kaos aaa'
+                    },
+                }
+                dataObject.forEach(element => {
+                    let data = `<li><a href="#detail-pesanan?id=${element.id}" target="_self" id="detail-pesanan" data-psn="${element.id}"><h2>${element.barang_cucian}</h2></a></li>`
+                    $('#list-pesanan').append(data)
+                });
 
-//----------------------------------------------------------------------------------
+                $('#list-pesanan').listview('refresh')
+            },
+            complete: function () {
+                $.mobile.loading('hide')
+            }
+        })
+    }
+}
