da55b25133a27c687eae668a1872c42b93d95364
Wildanokt <oktavianwww@gmail.com>
2019-11-25 14:10:46 +0700

    update halaman

--- pelanggan/js/script.js
+++ pelanggan/js/script.js
@@ -1,9 +1,48 @@
-$(document).on("pagecreate", "#page-one", function () {
-    $(document).on("swiperight", "#page-one", function (e) {
-        if ($(".ui-page-active").jqmData("panel") !== "open") {
-            if (e.type === "swiperight") {
-                $("#left-panel").panel("open");
-            }
+const link = 'http://localhost/';
+
+//-------------------------------- utility ----------------------------------------
+
+//panel swiper
+$(document).on("swiperight", function (e) {
+    if ($(".ui-page-active").jqmData("panel") !== "open") {
+        if (e.type === "swiperight") {
+            $("#left-panel").panel("open");
         }
-    });
+    }
 });
+
+//navigation
+$('#nav-home').on('click', function (e) {
+    e.preventDefault()
+    window.location.replace("./home.html")
+})
+$('#nav-profile').on('click', function (e) {
+    e.preventDefault()
+    window.location.replace("./profile.html")
+})
+
+//tambah pesanan
+$('#tambah-pesanan').on('click', function () {
+    alert('oioioi')
+})
+
+//--------------------------------- auth -------------------------------------------
+
+var check = {
+    authCheck: function () {
+        if (localStorage.getItem('status') == 0) {
+            window.location.replace("./index.html")
+        } else {
+            window.localStorage.replace("./home.html")
+        }
+    }
+}
+
+$('#form-login').submit(function () {
+    localStorage.setItem('status', 1)
+    window.location.replace("./home.html")
+})
+
+
+
+//----------------------------------------------------------------------------------
