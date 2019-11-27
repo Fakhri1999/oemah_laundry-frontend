3676535d2b65941dcb10aa54cc4fc83c8f0972d3
ilhamand12 <ilhamandriansah@gmail.com>
2019-11-27 14:57:31 +0700

    inshaAllah FIX

--- admin/js/app.js
+++ admin/js/app.js
@@ -1,107 +1,164 @@
-var dataObject = [{
-    "id_petugas": "0",
-    "nama": "ilham",
-    "username": "ilham",
-    "password": "qweasd123",
-    "tipe": "Petugas Cuci"
-}, {
-    "id_petugas": "1",
-    "nama": "baziyad",
-    "username": "baziyad@oemah.com",
-    "password": "123456789#",
-    "tipe": "Petugas Admin"
-}]
-
-var cuciObject = [{
-    "id_cuci": "1",
-    "nama": "jaket",
-    "harga": "3000",
-    "lama": "1",
-}, {
-    "id_cuci": "2",
-    "nama": "selimut",
-    "harga": "4000",
-    "lama": "2",
-}, {
-    "id_cuci": "3",
-    "nama": "kemeja",
-    "harga": "3000",
-    "lama": "1",
-}]
-
-
 var Application = {
     initApplication: function () {
         $(window).load('pageinit', '#page-admin', function () {
             Application.initShowAdm();
         })
         $(document).on('click', '#detail-admin', function () {
-            var id_admin = $(this).data('id');
+            var id_admin = $(this).data('temp');
             Application.initShowDetailAdm(id_admin);
+        })
+        $(document).on('click', '#submit-petugas', function () {
+            Application.initInsertAdm();
         })
         $(window).load('pageinit', '#page-cucian', function () {
             Application.initShowCuci();
         })
         $(document).on('click', '#detail-cucian', function () {
-            var id_cuci = $(this).data('id');
+            var id_cuci = $(this).data('temp');
             Application.initShowDetailCuci(id_cuci);
+        })
+        $(document).on('click', '#submit-cucian', function () {
+            Application.initInsertCuci();
         })
     },
 
     initShowAdm: function () {
-        var appendList = '';
-        dataObject.forEach(dataObject => {
-            appendList += `<li><a href="#page-detail-admin?id=${dataObject.id_petugas}" target="_self" id="detail-admin" data-id="${dataObject.id_petugas}">
-			<h2>${dataObject.nama}</h2>
-			<p>${dataObject.username}</p>
-            <p><b>${dataObject.tipe}</b></p>
-            </a></li>`
-        });
-        $('#list-admin').append(appendList);
+        $.ajax({
+            url: 'http://localhost/OemahLaundry-Backend/Petugas',
+            type: 'get',
+            success: function (res) {
+                var dataObject = res.data;
+                var appendList = '';
+                $("#list-admin").html('')
+                dataObject.forEach(dataObject => {
+                    let datapetugas = Object.keys(dataObject).map((val) => dataObject[val]).join(';')
+                    appendList += `<li><a href="#page-detail-admin?id=${dataObject.id_petugas}" target="_self" id="detail-admin" data-id="${dataObject.id_petugas}" data-temp="${datapetugas}">
+                    <h2>${dataObject.nama}</h2>
+                    <p>${dataObject.username}</p>
+                    <p><b>${dataObject.tipe}</b></p>
+                    </a></li>`
+                });
+                $('#list-admin').append(appendList);
+                $('#list-admin').listview('refresh');
+            }
+        })
     },
 
-    initShowDetailAdm: function (id) {
+    initShowDetailAdm: function (data) {
+        var dataObject = data.split(';');
         var appendDetail = "";
         var tbdy = $("#table-detailAdmin tbody");
         tbdy.html("");
-        dataObject.forEach(dataObject => {
-            if (dataObject.id_petugas == id) {
-                appendDetail = `<tr>
-            <td><b class="ui-table-cell-label">ID Petugas</b>${dataObject.id_petugas}</td>
-            <td><b class="ui-table-cell-label">Nama</b>${dataObject.nama}</td>
-            <td><b class="ui-table-cell-label">Username</b>${dataObject.username}</td>
-            <td><b class="ui-table-cell-label">Tipe Petugas</b>${dataObject.tipe}</td>
-            </tr>`
-            }
-        });
+        appendDetail = `<tr>
+        <td><b class="ui-table-cell-label">ID Petugas</b>${dataObject[0]}</td>
+        <td><b class="ui-table-cell-label">Nama</b>${dataObject[1]}</td>
+        <td><b class="ui-table-cell-label">Username</b>${dataObject[2]}</td>
+        <td><b class="ui-table-cell-label">Tipe Petugas</b>${dataObject[3]}</td>
+        </tr>`;
         $('#table-detailAdmin').append(appendDetail);
     },
-    initShowCuci: function () {
-        var appendList = '';
-        cuciObject.forEach(cuciObject => {
-            appendList += `<li><a href="#page-detail-cucian?id=${cuciObject.id_cuci}" target="_self" id="detail-cucian" data-id="${cuciObject.id_cuci}">
-			<h2>${cuciObject.nama}</h2>
-			<p>${cuciObject.harga}</p>
-            <p><b>${cuciObject.lama} hari</b></p>
-            </a></li>`
-        });
-        $('#list-barang').append(appendList);
+
+    initInsertAdm: function () {
+        var nama = $('#nama_pet').val();
+        var username = $('#username_pet').val();
+        var password = $('#pass_pet').val();
+        var tipe = $('#tipe-petugas :selected').val();
+        $.ajax({
+            url: 'http://localhost/OemahLaundry-Backend/Petugas',
+            type: 'post',
+            data: {
+                "nama": nama,
+                "username": username,
+                "password": password,
+                "tipe": tipe,
+                "usernameAdmin": "ilham",
+                "passwordAdmin": "andri"
+            },
+            success: function () {
+                Application.initShowAdm();
+                $("#popup-petugas").click()
+                setTimeout(function () {
+                    $("#p_pet").popup("close");
+                }, 3000);
+                setTimeout(function () {
+                    window.location.replace("#page-admin");
+                }, 3500);
+                setTimeout(function () {
+                    location.reload()
+                }, 4000);
+            },
+            error: function () {
+                alert("Field ada yang Kosong!!!")
+            }
+        })
     },
 
-    initShowDetailCuci: function (id) {
+    initShowCuci: function () {
+        $.ajax({
+            url: 'http://localhost/OemahLaundry-Backend/BarangCucian',
+            type: 'get',
+            success: function (res) {
+                var dataObject = res.data;
+                var appendList = '';
+                $("#list-barang").html('')
+                dataObject.forEach(dataObject => {
+                    let datacuci = Object.keys(dataObject).map((val) => dataObject[val]).join(';')
+                    appendList += `<li><a href="#page-detail-cucian?id=${dataObject.id}" target="_self" id="detail-cucian" data-id="${dataObject.id}" data-temp="${datacuci}">
+                    <h2>${dataObject.nama}</h2>
+                    <p>${dataObject.harga}</p>
+                    <p><b>${dataObject.lama} hari</b></p>
+                    </a></li>`
+                });
+                $('#list-cucian').append(appendList);
+                $('#list-cucian').listview('refresh');
+            }
+        })
+    },
+
+    initShowDetailCuci: function (data) {
+        var dataObject = data.split(';')
         var appendDetail = "";
         var tbdy = $("#table-detailCucian tbody");
         tbdy.html("");
-        cuciObject.forEach(cuciObject => {
-            if (cuciObject.id_cuci == id) {
-                appendDetail = `<tr>
-            <td><b class="ui-table-cell-label">ID cuci</b>${cuciObject.id_cuci}</td>
-            <td><b class="ui-table-cell-label">Nama</b>${cuciObject.nama}</td>
-            <td><b class="ui-table-cell-label">Harga</b>${cuciObject.harga}</td>
-            <td><b class="ui-table-cell-label">Lama</b>${cuciObject.lama} hari</td>
-            </tr>`
-            }
-        });
+        appendDetail = `<tr>
+                <td><b class="ui-table-cell-label">ID Barang</b>${dataObject[0]}</td>
+                <td><b class="ui-table-cell-label">Nama</b>${dataObject[1]}</td>
+                <td><b class="ui-table-cell-label">Harga</b>${dataObject[2]}</td>
+                <td><b class="ui-table-cell-label">Lama</b>${dataObject[3]} hari</td>
+                </tr>`;
         $('#table-detailCucian').append(appendDetail);
-    }
+    },
+
+    initInsertCuci: function () {
+        var barang = $('#nama_barang').val();
+        var harga = $('#harga_barang').val();
+        var lama = $('#lama_barang').val();
+        $.ajax({
+            url: 'http://localhost/OemahLaundry-Backend/BarangCucian',
+            type: 'post',
+            data: {
+                "barang": barang,
+                "harga": harga,
+                "lama": lama,
+                "usernameAdmin": "ilham",
+                "passwordAdmin": "andri"
+            },
+            success: function () {
+                Application.initShowCuci();
+                $("#popup-cucian").click()
+                setTimeout(function () {
+                    $("#p_cuci").popup("close");
+                }, 3000);
+                setTimeout(function () {
+                    window.location.replace("#page-cucian");
+                }, 3500);
+                $('#nama_barang').val('');
+                $('#harga_barang').val('');
+                $('#lama_barang').val('');
+            },
+            error: function () {
+                alert("Field ada yang Kosong!!!")
+            }
+        })
+    },
 };
