63ea705d662c78e4326e0abb6744f730b7324d92
Wildanokt <oktavianwww@gmail.com>
2019-11-27 13:21:11 +0700

    bismillah fix

--- pelanggan/js/script.js
+++ pelanggan/js/script.js
@@ -1,4 +1,4 @@
-const link = 'http://localhost/';
+const base_url = 'http://localhost/';
 
 //-------------------------------- utility ----------------------------------------
 
@@ -12,6 +12,8 @@ $(document).on("swiperight", function (e) {
 });
 
 //navigation
+$('#nav-name').html(`Hai, <b><u>${localStorage.getItem('username')}</u></b> !`)
+
 $('#nav-home').on('click', function (e) {
     e.preventDefault()
     window.location.replace("./home.html")
@@ -25,6 +27,9 @@ $('#nav-logout').on('click', function (e) {
     localStorage.setItem('status', 0)
     window.location.replace("./index.html")
 })
+$('#btn-back').on('click', function (event) {
+    window.location.replace("./profile.html")
+})
 
 //tambah pesanan
 $('#tambah-pesanan').on('click', function () {
@@ -33,6 +38,7 @@ $('#tambah-pesanan').on('click', function () {
 
 //--------------------------------- auth -------------------------------------------
 
+//check status login
 var check = {
     authCheck: function () {
         if (localStorage.getItem('status') == 0) {
@@ -41,23 +47,101 @@ var check = {
     }
 }
 
-$('#form-login').submit(function () {
+//login process
+$('#form-login').submit(function (event) {
+    event.preventDefault()
+    //input
     let username = $('input#username').val()
     let pass = $('input#pass').val()
 
+    //init component
+    let container = document.getElementById('notif')
+    container.innerHTML = ''
+    let grid_solo = document.createElement('div')
+    grid_solo.className = 'ui-grid-solo'
+    let grid_a = document.createElement('div')
+    grid_a.className = 'ui-grid-a'
+    let h4 = document.createElement('h4')
+    h4.className = 'center red'
 
-    localStorage.setItem('status', 1)
-    let data = {
-        'username': username,
+    //rule
+    if (username != '' && pass != '') {
+        console.log('tes')
+        localStorage.setItem('status', 1)
+        localStorage.setItem('username', username)
+        window.location.replace("./home.html")
+    } else {
+        //join
+        grid_a.appendChild(h4)
+        grid_solo.appendChild(grid_a)
+        container.appendChild(grid_solo)
+
+        h4.innerHTML = 'Mohon lengkapi seluruh data yang tersedia'
     }
-    localStorage.setItem('userdata', JSON.stringify(data))
-    //cara pakai => JSON.parse(localStorage.getItem('userdata'))
-    window.location.replace("./home.html")
+})
+
+$('#form-register').submit(function (event) {
+    event.preventDefault()
+    //input
+    let name = $('input#nama').val()
+    let username = $('input#username').val()
+    let pass1 = $('input#pass').val()
+    let pass2 = $('input#pass2').val()
+
+    //init component
+    let container = document.getElementById('notif')
+    container.innerHTML = ''
+    let grid_solo = document.createElement('div')
+    grid_solo.className = 'ui-grid-solo'
+    let grid_a = document.createElement('div')
+    grid_a.className = 'ui-grid-a'
+    let h4 = document.createElement('h4')
+    h4.className = 'center red'
+
+    //join
+    grid_a.appendChild(h4)
+    grid_solo.appendChild(grid_a)
+    container.appendChild(grid_solo)
+
+    //rule
+    if (name != '' && username != '' && pass1 != '' && pass2 != '') {
+        //all input filled
+        if (pass1.length >= 6) {
+            if (pass1 == pass2) {
+                //password matches
+                $data = {
+                    'nama': name,
+                    'username': username,
+                    'password': pass1
+                }
+                localStorage.setItem('status', 1)
+                localStorage.setItem('username', username)
+                window.location.replace('./home.html')
+            } else {
+                h4.innerHTML = 'Kedua password tidak cocok'
+            }
+        } else {
+            h4.innerHTML = 'Password minimal 6 karakter'
+        }
+    } else {
+        h4.innerHTML = 'Mohon lengkapi seluruh data yang tersedia'
+    }
+})
+
+$('#link-register').on('click', function (event) {
+    event.preventDefault()
+    window.location.replace('./register.html')
+})
+
+$('#link-login').on('click', function (event) {
+    event.preventDefault()
+    window.location.replace('./index.html')
 })
 
 //----------------------------------- pesanan ---------------------------------------
 localStorage.setItem('pesanan', 1)
 
+//tambah barang cucian
 $('#btn-tambah-barang').on('click', function (event) {
     event.preventDefault()
     localStorage.setItem('pesanan', (parseInt(localStorage.getItem('pesanan')) + 1))
@@ -69,19 +153,22 @@ $('#btn-tambah-barang').on('click', function (event) {
     block1.className = 'ui-block-a'
     let block2 = document.createElement('div')
     block2.className = 'ui-block-b'
+
     let input = document.createElement('input')
     input.type = 'number'
     input.id = 'jumlah-' + localStorage.getItem('pesanan')
     input.style = 'text-align: center'
     input.placeholder = "Jumlah"
+    input.value = '1'
     let divInput = document.createElement('div')
     divInput.className = 'ui-input-text ui-body-inherit ui-corner-all ui-shadow-inset'
-    let native = document.createAttribute("data-native-menu")
-    native.value = 'false'
-    input.setAttributeNode(native)
+
     let selectList = document.createElement('select')
     selectList.id = 'data-' + localStorage.getItem('pesanan')
     selectList.className = 'ui-btn ui-icon-carat-d ui-btn-icon-right ui-corner-all ui-shadow'
+    let native = document.createAttribute("data-native-menu")
+    native.value = 'false'
+    selectList.setAttributeNode(native)
 
     let options_str = ''
     let options = ['pakaian', 'selimut', 'boneka', 'seprei']
@@ -99,18 +186,26 @@ $('#btn-tambah-barang').on('click', function (event) {
     container.appendChild(block_container)
 })
 
+//pesanan process
 $('#form-pesanan').submit(function (event) {
     event.preventDefault()
-
+    let data = {}
     for (let i = 1; i <= parseInt(localStorage.getItem('pesanan')); i++) {
         let label = $('#data-' + i).val()
-        let jumlah = $('#jumlah-' + i).val()
-        console.log(`${label} = ${jumlah}`)
+        let jumlah = parseInt($('#jumlah-' + i).val())
+        if (data[label] != null) {
+            data[label] += jumlah
+        } else {
+            data[label] = jumlah
+        }
     }
+    console.log(data)
 })
 
+//dummy
 var pesanan = {
     load: function () {
+        let link = base_url
         $.ajax({
             url: link,
             type: 'get',
@@ -121,25 +216,24 @@ var pesanan = {
                 })
             },
             success: function (dataObject) {
-                $dataobject = {
-                    1: {
+                $data = [{
                         'id': 1,
                         'barang_cucian': 'kaos'
                     },
-                    2: {
+                    {
                         'id': 2,
                         'barang_cucian': 'kaos aaa'
                     },
-                    3: {
+                    {
                         'id': 3,
                         'barang_cucian': 'kaos'
                     },
-                    4: {
+                    {
                         'id': 4,
                         'barang_cucian': 'kaos aaa'
-                    },
-                }
-                dataObject.forEach(element => {
+                    }
+                ]
+                data.forEach(element => {
                     let data = `<li><a href="#detail-pesanan?id=${element.id}" target="_self" id="detail-pesanan" data-psn="${element.id}"><h2>${element.barang_cucian}</h2></a></li>`
                     $('#list-pesanan').append(data)
                 });
@@ -153,4 +247,27 @@ var pesanan = {
     }
 }
 
-//-----------------------------------------------------------------------------------
+//------------------------------------ propil --------------------------------------
+
+let profile = {
+    init: function () {
+        //setup profile
+    }
+}
+
+//button edit profile pressed
+$('#btn-edit-profile').on('click', function (event) {
+    event.preventDefault()
+    window.location.replace('./edit_profile.html')
+})
+
+//update profil data
+$('#edit-profile').submit(function (event) {
+    event.preventDefault()
+    let nama = $('input#nama').val()
+    let username = $('input#username').val()
+    let password = $('input#pass').val()
+    window.location.replace('./profile.html')
+})
+
+//----------------------------------------------------------------------------------
