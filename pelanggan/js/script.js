067a35254e5f27e0af8a6ceeb2225cb0e1df33be
baziyad48 <huhummamalybaziyad@gmail.com>
2019-11-28 15:05:37 +0700

    change to live server

--- pelanggan/js/script.js
+++ pelanggan/js/script.js
@@ -1,4 +1,4 @@
-const base_url = 'http://localhost/oemah_laundry-backend/';
+const base_url = 'https://oemah-laundry.herokuapp.com/';
 
 //-------------------------------- utility ----------------------------------------
 
